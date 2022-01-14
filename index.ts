412b2a0744c95a11f965c190271d11e2c4845789
bluekestrel <92190404+bluekestrel@users.noreply.github.com>
2022-01-13 22:18:33 -0500

    Added very simple ratelimit logic

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 "use strict";
 
 const Koa = require("koa");
+const ratelimit = require('koa-ratelimit');
 const helmet = require("koa-helmet");
 const body = require("koa-bodyparser");
 const cors = require("@koa/cors");
@@ -12,6 +13,24 @@ const powered = require("./middleware/powered");
 const router = require("./router");
 
 const index = new Koa();
+
+// setup simple in-memory ratelimit
+const db = new Map();
+index.use(ratelimit({
+  driver: 'memory',
+  db: db,
+  duration: 60000, // 60 seconds in milliseconds
+  errorMessage: 'Rate limit exceeded, please wait one minute',
+  id: (ctx) => ctx.ip,
+  headers: {
+    remaining: 'Rate-Limit-Remaining',
+    reset: 'Rate-Limit-Reset',
+    total: 'Rate-Limit-Total'
+  },
+  max: 80, // 80 requests per minute allowed, if new routes are added that allow for multiple
+           // values to be requested, the ratelimit should be lowered dramatically
+  disableHeader: false,
+}));
 
 index.use(rt);
 index.use(conditional());
